9f471c82a219a6d18692067deac4784381de17bd
Saleemgaro2017 <95593101+Saleemgaro2017@users.noreply.github.com>
2025-05-07 01:09:30 +0100

    Create Task2.kt

---
+++ Task2.kt
@@ -0,0 +1,7 @@
+fun main() {
+    print("Enter your name: ")
+    val name = readLine()
+    print("Enter your age: ")
+    val age = readLine()
+    println("Hello $name, you are $age years old!")
+}
